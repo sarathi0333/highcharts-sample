b43ae3952b1d9578df7e749f6847e41fc6aa4bb7
Sarath <sarath.kct@gmail.com>
2024-01-08 16:14:17 +0530

    Added highchart sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'highcharts-sample';
+
+  ngAfterViewInit() {
+    const data = [25, 10, 34];
+    Highcharts.chart('chart', {
+      title: {
+        text: "sample chart"
+      },
+      series: [{
+        data: data,
+        type: 'line'
+      }]
+    })
+  }
+
+
 }
